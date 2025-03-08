13971af39236d87c14d2e355bcca0edf5146c983
Zachary Dremann <dremann@gmail.com>
2025-03-08 15:04:12 -0500

    chore: update bindings to CRoaring 4.3.0

--- croaring-sys/CRoaring/roaring.hh
+++ croaring-sys/CRoaring/roaring.hh
@@ -1,5 +1,5 @@
 // !!! DO NOT EDIT - THIS IS AN AUTO-GENERATED FILE !!!
-// Created by amalgamation.sh on 2025-02-26T22:28:04Z
+// Created by amalgamation.sh on 2025-02-28T15:35:21Z
 
 /*
  * The CRoaring project is under a dual license (Apache/MIT).
